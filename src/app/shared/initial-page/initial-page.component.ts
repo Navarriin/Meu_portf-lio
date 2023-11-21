45d06c8ae38c5d8871eede6a6144dc734fcd019c
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-21 14:42:23 -0300

    mudança de component

---
+++ src/app/shared/initial-page/initial-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-initial-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './initial-page.component.html',
+  styleUrl: './initial-page.component.scss'
+})
+export class InitialPageComponent {
+
+}
