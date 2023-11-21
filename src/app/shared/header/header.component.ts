f08712ae9e8c75609e40ade344c45dfab3c21fbf
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-20 22:16:11 -0300

    funçao que pinta de vermelho onde eu clicar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -11,14 +11,37 @@ import { CommonModule } from '@angular/common';
 export class HeaderComponent {
   constructor(private renderer: Renderer2, private element: ElementRef) {}
 
+  ngOnInit() {
+    this.ClickMenu();
+  }
+
   toggleMenu(): void {
-    const nav: HTMLElement | null =
+    const active: HTMLElement =
       this.element.nativeElement.querySelector('#nav');
 
-    if (nav?.classList.contains('activate')) {
-      this.renderer.removeClass(nav, 'activate');
+    if (active.classList.contains('activate')) {
+      this.renderer.removeClass(active, 'activate');
     } else {
-      this.renderer.addClass(nav, 'activate');
+      this.renderer.addClass(active, 'activate');
     }
+  }
+
+  ClickMenu(): void {
+    const buttons: NodeListOf<HTMLElement> =
+      this.element.nativeElement.querySelectorAll('.button');
+
+    buttons.forEach((button: HTMLElement) => {
+      button.addEventListener('click', () => {
+        this.removeButton();
+        this.renderer.addClass(button, 'select');
+      });
+    });
+  }
+
+  removeButton(): void {
+    const remove: HTMLElement | null =
+      this.element.nativeElement.querySelector('.button.select');
+
+    this.renderer.removeClass(remove, 'select');
   }
 }
