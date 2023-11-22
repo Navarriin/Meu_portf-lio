66ceda266ab5977df7e09cfee13f291c4d948a93
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-22 17:38:19 -0300

    deixando o component dinamico com o data criado

--- src/app/shared/projects/project/project.component.ts
+++ src/app/shared/projects/project/project.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Projects } from '../../../data/projects.data';
 
 @Component({
   selector: 'project',
@@ -8,4 +9,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss',
 })
-export class ProjectComponent {}
+export class ProjectComponent {
+  @Input() id: string = '0';
+  protected projectImage: string = '';
+  protected projectTitle: string = '';
+  protected projectdescription: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.setValuesComponents(this.id);
+  }
+
+  setValuesComponents(id: string | null): void {
+    const values = Projects.filter((value) => value.id === id)[0];
+    this.projectImage = values.image;
+    this.projectTitle = values.title;
+    this.projectdescription = values.description;
+  }
+}
