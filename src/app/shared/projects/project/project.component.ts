968a8bcd10e183ec8be8399d8b576e8ae8ae3b7b
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-22 17:13:48 -0300

    termino do componente project/projects

---
+++ src/app/shared/projects/project/project.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'project',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './project.component.html',
+  styleUrl: './project.component.scss',
+})
+export class ProjectComponent {}
--- src/app/shared/projects/projects.component.ts
+++ src/app/shared/projects/projects.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 // Meu component
 import { DivisorComponent } from '../divisor/divisor.component';
+import { ProjectComponent } from './project/project.component';
 
 @Component({
   selector: 'projects',
   standalone: true,
-  imports: [CommonModule, DivisorComponent],
+  imports: [CommonModule, DivisorComponent, ProjectComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
