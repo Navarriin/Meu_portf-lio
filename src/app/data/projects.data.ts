4a8b03ee8bc19f17ddff2c90bdb782e41ecfdea6
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-22 17:37:53 -0300

    criaçao de data para armazenar dados dos projetos

---
+++ src/app/data/projects.data.ts
@@ -0,0 +1,18 @@
+export const Projects = [
+  {
+    id: '1',
+    image: 'assets/prints/site_pokemon.png',
+    title: 'NavaMon',
+    description:
+      'Este projeto é um site onde você pode escolher o pokemon que quiser ver os seus atributos.',
+  },
+  {
+    // Default
+    id: '2',
+    image:
+      'https://gifs.eco.br/wp-content/uploads/2023/08/imagens-de-em-breve-png-1.png',
+    title: 'Projeto em andamento.',
+    description:
+      'Aqui é onde ira ficar algum projeto meu assim que ficar completo.',
+  },
+];
