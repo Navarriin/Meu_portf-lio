88e097361101da27c0b2a13f22bb1464e585d54d
Gabriel <gabrielnavarro.vip@gmail.com>
2023-11-27 16:40:10 -0300

    adicionando novos projetos no porfolio

--- src/app/data/projects.data.ts
+++ src/app/data/projects.data.ts
@@ -11,6 +11,26 @@ export const Projects = [
   {
     // Default
     id: '2',
+    title: 'Spotifyyy',
+    description:
+      'Meu projeto onde eu crio o meu próprio spotify para praticar CRUD.',
+    repository: 'https://github.com/Navarriin/Sporifyyy',
+    ar: 'https://github.com/Navarriin/Sporifyyy',
+    target: '_self',
+  },
+  {
+    // Default
+    id: '3',
+    title: 'API do Spotifyyy',
+    description:
+      'Projeto da API que fiz para o CRUD do meu Spotifyyy com Java e Spring',
+    repository: 'https://github.com/Navarriin/API_spotfy',
+    ar: 'https://github.com/Navarriin/API_spotfy',
+    target: '_self',
+  },
+  {
+    // Default
+    id: '4',
     title: 'Em Breve',
     description:
       'Aqui é onde ira ficar algum projeto meu assim que ficar completo.',
